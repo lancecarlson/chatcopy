cc7a8e056d062907b6c86ffa5ea51e47089d1b54
Lance Carlson <lancecarlson@gmail.com>
2023-06-27 21:31:07 -0400

    help menu works

--- src/cli.cr
+++ src/cli.cr
@@ -6,28 +6,39 @@ module Chatcopy
     DEFAULT_PATH  = "prompt.md"
     DEFAULT_MODEL = "gpt-3.5-turbo-0613"
 
-    getter file_path : String
-    getter model : String
-
-    def initialize
-      @file_path = ARGV[0]? || DEFAULT_PATH
-    end
-
+    # Set default values for file path and model on initialization
     def initialize
       @file_path = DEFAULT_PATH
       @model = DEFAULT_MODEL
-      OptionParser.new do |parser|
-        parser.banner = "Usage: chatcopy [path to file]"
 
-        parser.on("-m MODEL", "--model=MODEL", "Specify the OpenAI model to use") { |model| @model = model }
+      # Parse any command line arguments
+      parse_arguments
+    end
+
+    # Define how the command line arguments are parsed
+    def parse_arguments
+      parser = OptionParser.new do |parser|
+        parser.banner = "Usage: chatcopy [options] [path to file]"
+
+        parser.on("-m MODEL", "--model=MODEL", "Specify the OpenAI model to use") do |model|
+          @model = model
+        end
+
         parser.on("-h", "--help", "Show this help") do
           puts parser
           exit
         end
       end
+
+      parser.parse(ARGV)
+
+      # If a non-option argument is provided, it is the file path
+      @file_path = ARGV[0] if ARGV[0]?
     end
 
+    # Define the main functionality of the CLI
     def run
+      # Then, run the main application logic
       prompt = File.read(@file_path)
       file_tree = FileTree.new
       output = FileSearcher.new(prompt, file_tree, @model).relevant_code
@@ -36,5 +47,6 @@ module Chatcopy
     end
   end
 
-  CLI.new.run
+  # Create a new CLI instance and run it
+  cli = Chatcopy::CLI.new.run
 end
